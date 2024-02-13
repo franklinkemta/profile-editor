439d9197afe802220dc72b98a34e2074ef193aea
franklinkemta <franklinkemta@gmail.com>
2024-02-13 06:07:20 -0500

    add resume enhancer on the /editor endpoint, add prompt structure, and request to openai api

---
+++ src/utils/projects.ts
@@ -0,0 +1,15 @@
+
+export const projects: CardData[] = [
+    {
+        name: "Cloud to Edge services migration to reduce usage Cost, improve Performance and Scalability",
+        description: "",
+        backgroundImg: "/images/projects/devops/cloud_to_edge_devOps_migration.jpg"
+    },
+    {
+        name: "Full stack development, realtime road traffic analysis",
+        description: "",
+        backgroundImg: "/images/projects/fullstack/ViizeTraffic.jpg"
+    }
+]
+
+export default projects
